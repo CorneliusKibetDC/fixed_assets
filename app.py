de8f0b0566cc22db9e42c18798fe368434aa404f
CorneliusKibetDC <kapslogo18@gmail.com>
2025-02-25 10:53:35 +0300

    added ip in db configuration

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Supabase Database Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://p-admin:inventory2030@localhost:5432/db?sslmode=require'
+# Database Configuration for Linode PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://p-admin:inventory2030@172.236.2.18:5432/db?sslmode=require'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "Connected to Supabase PostgreSQL!"
+    return "Connected to PostgreSQL on Linode!"
+
+# Import routes from manage_fixed_asset_assignment after defining the app and db
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
