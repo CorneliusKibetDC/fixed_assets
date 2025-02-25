e9b6eb06eb8f89112e88d6237fc3c2d5e7e9f595
+ <+>
2025-02-25 10:43:01 +0300

    edited add, delete, filter,delete

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
 
 app = Flask(__name__)
+
+api=Api(app,doc='/docs')
 
 # Supabase Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://p-admin:inventory2030@localhost:5432/db?sslmode=require'
